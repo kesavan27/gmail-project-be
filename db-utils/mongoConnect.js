cea5e71ba8e1b71d5d50b7ccc42520329994965c
kesavan27 <gvkesavan2003@gmail.com>
2024-10-01 18:52:33 +0530

    mongo update

--- db-utils/mongoConnect.js
+++ db-utils/mongoConnect.js
@@ -7,7 +7,7 @@ const dbCluster = process.env.DB_CLUSTER || "";
 
 const dbName = process.env.DB_NAME || "";
 
-const dbUserName = process.env.DB_USER || "";
+const dbUserName = process.env.DB_USERNAME || "";
 
 const dbPassword = process.env.DB_PASSWORD || "";
 
